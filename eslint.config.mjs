4db018da001773170dab688005ae7e582355e4a3
Dave Birch <dave@oneday.com>
2025-08-09 10:48:42 -0400

    consolidate eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
 import prettierRecommended from 'eslint-config-prettier';
 import vue from 'eslint-plugin-vue';
-import tseslint from 'typescript-eslint'; // ✅ meta package
+import tseslint from 'typescript-eslint';
 import vueParser from 'vue-eslint-parser';
 import withNuxt from './.nuxt/eslint.config.mjs';
 
 const config = withNuxt([
-    // Vue SFCs
     {
-        files: ['**/*.vue'],
+        files: ['**/*.{vue,ts}'],
         languageOptions: {
             parser: vueParser,
             parserOptions: {
@@ -27,25 +26,6 @@ const config = withNuxt([
             'no-undef': 'error',
             'vue/multi-word-component-names': 'off',
             'vue/no-v-html': 'off',
-            quotes: ['error', 'single', { allowTemplateLiterals: true }],
-            semi: ['error', 'always'],
-            '@typescript-eslint/no-explicit-any': ['error', { fixToUnknown: true }],
-        },
-    },
-
-    // Plain TS files
-    {
-        files: ['**/*.{ts,tsx}'],
-        languageOptions: {
-            parserOptions: {
-                ecmaVersion: 'latest',
-                sourceType: 'module',
-            },
-        },
-        plugins: {},
-        rules: {
-            ...prettierRecommended.rules,
-            eqeqeq: ['error', 'always'],
             quotes: ['error', 'single', { allowTemplateLiterals: true }],
             semi: ['error', 'always'],
             '@typescript-eslint/no-explicit-any': ['error', { fixToUnknown: true }],

c62acb06146553a4a9e05a3c139912cc79c1019a
Dave Birch <dave@oneday.com>
2025-08-16 18:03:34 -0500

    hero

---
+++ app/layouts/brochure.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <MainMenu />
+        <slot name="splash">
+            <HeroSplash v-if="splash" v-bind="splash" />
+        </slot>
+        <UMain>
+            <UContainer>
+                <slot />
+            </UContainer>
+        </UMain>
+        <SiteFooter />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import HeroSplash from '@/components/hero-splash.vue';
+
+    const route = useRoute();
+    const splash = computed(() => route.meta.splash as any | undefined);
+</script>
